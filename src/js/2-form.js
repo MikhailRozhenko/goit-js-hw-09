d40f119358a935993817eac6e2bc85fbdbaaf514
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2025-07-31 21:18:18 +0300

    from

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,36 +4,38 @@ let formData = {
 };
 
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', e => {
-  const email = e.currentTarget.elements.email.value;
-  const message = e.currentTarget.elements.message.value;
-  formData.email = email.trim();
-  formData.message = message.trim();
-  saveToLS('feedback-form-state', formData);
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   const lsData = getFromLS('feedback-form-state');
-  try {
+
+  if (lsData) {
     formData = lsData;
-    form.elements.email.value = lsData.email;
-    form.elements.message.value = lsData.message;
-  } catch {}
-});
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  const email = form.elements.email.value.trim();
-  const message = form.elements.message.value.trim();
-
-  if (email === '' || message === '') {
-    alert('Fill please all fields');
-  } else {
-    console.log(formData);
-    form.reset();
-    localStorage.removeItem('feedback-form-state');
-    formData = { email: '', message: '' };
+    form.elements.email.value = lsData.email || '';
+    form.elements.message.value = lsData.message || '';
   }
+
+  form.addEventListener('input', e => {
+    const email = e.currentTarget.elements.email.value;
+    const message = e.currentTarget.elements.message.value;
+    formData.email = email.trim();
+    formData.message = message.trim();
+    saveToLS('feedback-form-state', formData);
+  });
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = form.elements.email.value.trim();
+    const message = form.elements.message.value.trim();
+
+    if (email === '' || message === '') {
+      alert('Fill please all fields');
+    } else {
+      console.log(formData);
+      form.reset();
+      localStorage.removeItem('feedback-form-state');
+      formData = { email: '', message: '' };
+    }
+  });
 });
 
 function saveToLS(key, value) {
@@ -41,13 +43,15 @@ function saveToLS(key, value) {
   localStorage.setItem(key, jsonData);
 }
 
-function getFromLS(key, defaultValue) {
+function getFromLS(key, defaultValue = { email: '', message: '' }) {
   const jsonData = localStorage.getItem(key);
+  if (!jsonData) return defaultValue;
+
   try {
     const data = JSON.parse(jsonData);
-    return data;
+    return data ?? defaultValue;
   } catch {
-    return defaultValue || jsonData;
+    return defaultValue;
   }
 }
 
